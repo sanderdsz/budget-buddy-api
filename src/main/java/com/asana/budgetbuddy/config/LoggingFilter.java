b0a822a9a8da672eec7cfbc9bc1a644194688169
sander <sanderdsz@gmail.com>
2023-05-08 21:10:00 -0300

    WIP: logging feature

--- src/main/java/com/asana/budgetbuddy/config/LoggingFilter.java
+++ src/main/java/com/asana/budgetbuddy/config/LoggingFilter.java
@@ -3,41 +3,66 @@ package com.asana.budgetbuddy.config;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.Enumeration;
 
-//@Component
+@Component
 public class LoggingFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggerInterceptor.class);
 
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-
-        HttpServletRequest request = (HttpServletRequest) req;
-        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request);
-        try {
-            chain.doFilter(wrappedRequest, res);
-        } finally {
-            logRequestBody(wrappedRequest);
-        }
+    private static void requestLoggerBuilder(ContentCachingRequestWrapper request) {
+        JSONObject body = stringToJsonBody(request);
+        JSONObject headers = stringToJsonHeaders(request);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("type", "request");
+        jsonObject.put("body", body);
+        jsonObject.put("headers", headers);
+        logger.info(jsonObject.toString(4));
     }
 
-    private static void logRequestBody(ContentCachingRequestWrapper request) {
+    private static JSONObject stringToJsonHeaders(ContentCachingRequestWrapper request) {
+        JSONObject headers = new JSONObject();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            String headerValue = request.getHeader(headerName);
+            headers.put(headerName, headerValue);
+        }
+        return headers;
+    }
 
+    private static JSONObject stringToJsonBody(ContentCachingRequestWrapper request) {
         byte[] buf = request.getContentAsByteArray();
         if (buf.length > 0) {
             try {
                 String requestBody = new String(buf, 0, buf.length, request.getCharacterEncoding());
-                logger.info("body=[" + requestBody + "]");
+                return new JSONObject(requestBody);
             } catch (Exception e) {
                 System.out.println("error in reading request body");
             }
+        }
+        return null;
+    }
+
+    public void doFilter(
+            ServletRequest req,
+            ServletResponse res,
+            FilterChain chain
+    ) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request);
+        try {
+            chain.doFilter(wrappedRequest, response);
+        } finally {
+            requestLoggerBuilder(wrappedRequest);
         }
     }
 }
