3ef097b4a9e043778d4ca1002954a54d7e06fc06
sander <sanderdsz@gmail.com>
2023-05-11 11:56:07 -0300

    chore: logging filter unit test

---
+++ src/test/java/com/asana/budgetbuddy/config/LoggingFilterTest.java
@@ -0,0 +1,23 @@
+package com.asana.budgetbuddy.config;
+
+import jakarta.servlet.ServletException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.io.IOException;
+
+@SpringBootTest
+public class LoggingFilterTest {
+
+    @Test
+    void testDoFilter() throws ServletException, IOException {
+        LoggingFilter filter = new LoggingFilter();
+        MockFilterChain mockFilterChain = new MockFilterChain();
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        MockHttpServletResponse mockHttpServletResponse = new MockHttpServletResponse();
+        filter.doFilter(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+    }
+}
