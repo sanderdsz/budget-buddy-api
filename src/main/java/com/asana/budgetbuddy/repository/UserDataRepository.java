4e209043a02f8ecf559e0f1c089531022350e804
sander <sanderdsz@gmail.com>
2023-05-15 21:05:32 -0300

    Revert "feat: AES256 password encription added"
    This reverts commit d27223db32605bb03068eb59a94ee4f5af27b068.

--- src/main/java/com/asana/budgetbuddy/repository/UserDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.asana.budgetbuddy.repository;
-
-import com.asana.budgetbuddy.model.UserData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDataRepository extends JpaRepository<UserData, Long> {
-}
