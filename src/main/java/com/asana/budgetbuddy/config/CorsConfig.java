d768d88f76d18f9afc313adfd7534b03d66ceaaf
sanderdsz <sanderdsz@gmail.com>
2023-06-11 10:16:28 -0300

    fix: cors with filters

--- src/main/java/com/asana/budgetbuddy/config/CorsConfig.java
+++ src/main/java/com/asana/budgetbuddy/config/CorsConfig.java
@@ -1,22 +1,22 @@
-package com.asana.budgetbuddy.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedOriginPatterns("*"); // TODO: PUT FRONT END URL
-            }
-        };
-    }
-}
+//package com.asana.budgetbuddy.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig {
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+//                        .allowedOrigins("http://localhost:3000")
+//                        .allowedOriginPatterns("http://localhost:3000"); // TODO: PUT FRONT END URL
+//            }
+//        };
+//    }
+//}
--- src/main/java/com/asana/budgetbuddy/controller/AuthController.java
+++ src/main/java/com/asana/budgetbuddy/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
