975d539e53649db6fd862d39ec9ffcb797fabaf4
Sander dos Santos Zuchinalli <sanderdsz@gmail.com>
2025-01-20 20:47:41 -0300

    Update backup.sh
    feat: update bash script backup file

--- backup.sh
+++ backup.sh
@@ -14,16 +14,18 @@ echo "Today: $current_date"
 yesterday_date=$(date -d "$today_date - 1 day" +%Y%m%d)
 echo "Yesterday: $yesterday_date"
 
-backup_filename="backup_${yesterday_date}.sql"
+old_backup_filename="backup_${yesterday_date}.sql"
+echo "Yesterday backup file: $old_backup_filename"
 
 # Check if the backup file with yesterday's date exists
-if [ -f "$backup_filename" ]; then
+if [ -f "$old_backup_filename" ]; then
   # Delete the older backup
-  rm "$backup_filename"
-  echo "Old backup deleted: $backup_filename"
+  rm -rf "$old_backup_filename"
+  echo "Old backup deleted: $old_backup_filename"
 fi
 
 backup_filename="backup_${current_date}.sql"
+echo "Current backup file: $backup_filename"
 
 echo "Starting database dump..."
 # Run the docker exec command to perform a new dump inside the MYSQL container
@@ -33,6 +35,9 @@ echo "Database dump performed inside docker container"
 # Run the docker cp command to perform a new backup
 sudo docker cp budget-buddy-db:/backup.sql "$backup_filename"
 echo "New backup performed: $backup_filename"
+
+echo "Moving the backup into the Google Drive folder..."
+mv $backup_filename /home/ubuntu/backup/Linux/backup
 
 # To import the backup to MYSQL
 # mysql -u user -ppassword budget-buddy < /backup.sql
